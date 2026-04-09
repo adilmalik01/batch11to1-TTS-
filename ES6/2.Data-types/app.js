b28ebf7242a95cf89bd34fc916f06dbb30c76af1
Muhammad Adil <malikadil5679@gmail.com>
2026-04-09 12:11:11 +0500

    Add initial HTML structure and JavaScript files for data types and variables4

---
+++ ES6/2.Data-types/app.js
@@ -0,0 +1,29 @@
+// console.log("Hello World");
+
+
+// String 
+console.log("hello World 456789")
+// ""
+// ''
+
+
+
+// Number 
+console.log(1234567890);
+
+// Boolean
+console.log(false);
+console.log(true);
+
+
+
+// Null
+console.log(null);
+
+
+// Undefined
+console.log(undefined);
+
+
+// Object
+// Array
---
+++ ES6/3.Variables/app.js
@@ -0,0 +1,98 @@
+
+
+// var my_name = "Ali"
+
+// console.log(my_name);
+// console.log(my_name);
+// console.log(my_name);
+// console.log(my_name);
+// console.log(my_name);
+// console.log(my_name);
+// console.log(my_name);
+// console.log(my_name);
+// console.log(my_name);
+// console.log(my_name);
+// console.log(my_name);
+// console.log(my_name);
+// console.log(my_name);
+// console.log(my_name);
+
+
+// console.log("Ali");
+// console.log("Adil");
+// console.log("Adil");
+// console.log("Adil");
+// console.log("Adil");
+// console.log("Adil");
+// console.log("Adil");
+// console.log("Adil");
+// console.log("Adil");
+// console.log("Adil");
+// console.log("Adil");
+// console.log("Adil");
+// console.log("Adil");
+// console.log("Adil");
+// console.log("Adil");
+// console.log("Adil");
+// console.log("Adil");
+// console.log("Adil");
+
+
+
+// var
+// let 
+// const 
+
+
+
+
+
+
+// var var_variable = "This is var variable"
+// console.log(var_variable);
+
+
+// let letVariable = "This is let variable"
+// console.log(letVariable)
+
+// const constvariable = "This is Const Variable"
+// console.log(constvariable)
+
+
+
+// var num = 10
+// console.log(num)
+ 
+// var num = 20
+// console.log(num)
+
+
+
+// let num = 10
+// console.log(num)
+ 
+// num = 20
+// console.log(num)
+
+
+
+
+
+const num = 10
+console.log(num)
+ 
+num = 20
+console.log(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
