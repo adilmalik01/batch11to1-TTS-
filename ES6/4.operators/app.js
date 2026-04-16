e5f7a90da50538238a009a089863e077a5882339
Muhammad Adil <malikadil5679@gmail.com>
2026-04-16 11:57:21 +0500

    Add initial HTML and JavaScript files for operators demonstration

---
+++ ES6/4.operators/app.js
@@ -0,0 +1,91 @@
+
+
+// Arithmetic Operators
+// let num1 = 10;
+// let num2 = 5;
+// let sum = num1 + num2;
+// + - * /
+// console.log(sum);
+
+
+
+
+
+
+// Assignment Operators
+
+// let num1 = 10;
+// let num2 = 5;
+
+// let sum = num1 + num2; 
+
+// sum += 10
+
+// console.log(sum);
+
+
+
+// Comparison Operators
+
+// == // Equal to
+// console.log("adil" == "adil");
+
+// === // Strict equal to
+// console.log(5 === "5");
+
+// != // Not equal to
+// console.log(5 != 5);
+
+
+// > // Greater than
+// console.log(5 > 6);
+
+
+
+// < // Less than
+// console.log(5 < 4);
+
+
+
+// >= // Greater than or equal to
+// console.log(5 >= 5);
+
+// <= // Less than or equal to
+// console.log(5 <=  5);
+
+
+
+
+
+// Logical Operators
+
+// && // Logical AND
+// || // Logical OR
+
+
+
+
+// console.log(5 < 3);
+let email = "adil@gmail.com"
+let password = 12345
+
+// console.log(email == "adil@gmail.com" && password == 12345 &&  email.length > 5);
+console.log(email == "adil@gmail.com" || password == 1234);
+
+
+
+// console.log(password == 12345);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
