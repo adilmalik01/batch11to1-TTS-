388e0137072a88d1c66f30d92b63549be410487b
Muhammad Adil <malikadil5679@gmail.com>
2026-05-07 11:52:39 +0500

    Update student array and comment out array method examples in app.js

--- ES6/8.Array_Methods/app.js
+++ ES6/8.Array_Methods/app.js
@@ -7,7 +7,7 @@
 
 
 
-let students = ["Ali","Hamza", "Hassan", "Hussain"]
+let students = ["Ali","Hamza", "Hassan", "Hussain","Adil","Aqib"]
 // console.log(students[2]);
 // console.log(students[3]);
 // console.log(students[0]);
@@ -16,20 +16,49 @@ let students = ["Ali","Hamza", "Hassan", "Hussain"]
 
 
 // push 
-students.push("Aqib")
-students.push("Hello")
-console.log(students);
+// students.push("Aqib")
+// students.push("Hello")
+// console.log(students);
 
 
 // pop
-students.pop()
-students.pop()
-console.log(students);
+// students.pop()
+// console.log(students);
+
+
+// unshift
+// console.log(students);
+// students.unshift("Aqib","Hamza")
+// console.log(students);
+
+
+
 
 // shift
-// unshift
-// splice
+// students.shift()
+// students.shift()
+// console.log(students)
+
 // slice
+
+
+// let copy_names = students.slice(2,4)
+// console.log(copy_names);
+
+
+
+
+// splice
+
+
+// console.log(students);
+// students.splice(3,1,"hello")
+// console.log(students);
+
+
+
+
+
 
 
 
