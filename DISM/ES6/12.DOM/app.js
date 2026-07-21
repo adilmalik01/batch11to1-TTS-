364aae564e7a35a98a248bbef6d5836501dc466c
adilmalik01 <malikadil5679@gmail.com>
2026-07-21 12:02:50 +0500

    Add initial HTML and JavaScript files for DOM manipulation demonstration

---
+++ DISM/ES6/12.DOM/app.js
@@ -0,0 +1,45 @@
+
+
+
+
+let h1Tag = document.querySelector(".hello")
+let imgTag = document.querySelector("#img")
+let buttonTag = document.querySelector("#btn")
+
+
+
+buttonTag.addEventListener("click", () => {
+    h1Tag.innerHTML = "Hallo Adil"
+    imgTag.src = "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png"
+
+})
+
+
+// console.log(h1Tag.innerHTML);
+
+
+
+
+// console.log(document);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
